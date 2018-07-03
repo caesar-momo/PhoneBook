488b69e0dfe86a7adbedd811191d0dfc1c693101
caesar <linmomo91@outlook.com>
2018-07-04 00:04:59 +0800

    Vue full-stack Save data & commit phonebook

---
+++ resources/assets/js/components/Myfooter.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer class="footer">
+        <div class="content has-text-centered">
+            <p>
+            <strong>Vuejs Phonebook App</strong> by <a href="https://jgthms.com">Lin MoCheng</a>.Access the source code
+            <a href="http://github.com/caesar-momo/phonebook"> phonebook</a>.
+            </p>
+            <p>
+                <a href="https://github.com/caesar-momo/" class="icon">
+                    <i class="fa fa-github"></i>
+                </a>
+            </p>
+        </div>
+    </footer>
+</template>
